c13d6516509994a7f59b6a9f725adf6448f0b4c3
Sinchan Nandy <105705266+BetaBot2002@users.noreply.github.com>
2024-08-02 03:40:41 +0530

    Home Page updated

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import "../Styles/CSS/home.css"
 import NavBar from '../Components/NavBar'
 import HungerPlate from "../assets/HungerPlate.png"
+import WorldMap from "../assets/WorldMap.png"
+import MealPlanner from "../assets/MealPlanner.png"
+import ExpirationTracker from "../assets/ExpirationTracker.png"
+import RecipeSuggestion from "../assets/RecipeSuggestion.png"
+import Donation from "../assets/Donation.png"
 
 const Home = () => {
     return (
@@ -29,16 +34,44 @@ const Home = () => {
                 </div>
                 <div className="callToActionContainer">
                     <div className="callToAction">
-                        Meal Planning
+                        <div className="callToActionImage">
+                            <img src={MealPlanner} alt="" />
+                        </div>
+                        <div className="callToActionText">
+                            Meal Planner
+                        </div>
                     </div>
                     <div className="callToAction">
-                        Expiration Tracker
+                        <div className="callToActionImage">
+                            <img src={ExpirationTracker} alt="" />
+                        </div>
+                        <div className="callToActionText">
+                            Expiration Tracker
+                        </div>
                     </div>
                     <div className="callToAction">
-                        Recipe Suggestions
+                        <div className="callToActionImage">
+                            <img src={RecipeSuggestion} alt="" />
+                        </div>
+                        <div className="callToActionText">
+                            AI Recipe Suggestion
+                        </div>
                     </div>
                     <div className="callToAction">
-                        Donation Coordination
+                        <div className="callToActionImage">
+                            <img src={Donation} alt="" />
+                        </div>
+                        <div className="callToActionText">
+                            Donation
+                        </div>
+                    </div>
+                </div>
+                <div className="mapContainer">
+                    <div className="mapText">
+                        Make The World A Better Place
+                    </div>
+                    <div className="mapImage">
+                        <img src={WorldMap} alt="" />
                     </div>
                 </div>
             </div>
