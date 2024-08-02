3358537e5d560f5db028a965d2c0b156907d4dbf
Sinchan Nandy <105705266+BetaBot2002@users.noreply.github.com>
2024-08-03 03:44:48 +0530

    system instructions updated

--- Frontend/src/utils/geminiModels.js
+++ Frontend/src/utils/geminiModels.js
@@ -1,7 +1,8 @@
 import { GoogleGenerativeAI } from "@google/generative-ai"
-import { mealModelSystemInstructions, recipeModelSystemInstruction } from "./geminiInstructions";
 
 const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY);
+const recipeModelSystemInstruction=import.meta.env.VITE_RECIPE_SYSTEM_INSTRUCTION
+const mealModelSystemInstructions=import.meta.env.VITE_MEAL_SYSTEM_INSTRUCTION
 
 const recipeModel = genAI.getGenerativeModel({
     model: "gemini-1.5-flash",
